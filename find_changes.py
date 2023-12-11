f86f03d621925c03290908de27a58604c6bcd772
Cristian FIODOROV <cfiodorov@bitdefender.biz>
2023-12-11 18:04:21 +0200

    resolved a bug in find_changes

--- find_changes.py
+++ find_changes.py
@@ -10,7 +10,7 @@ class ModificationType(Enum):
 
 
 def find_changes(location_func: LocationFunc, files_map: dict, number: int) -> List[Tuple]:
-    result = []
+    result_mod, result_del, result_mk = [], [], []
     items1 = [*files_map.items()]
     items2 = location_func.get_paths()
 
@@ -18,15 +18,17 @@ def find_changes(location_func: LocationFunc, files_map: dict, number: int) -> L
         for item in items2:
             if item.name == file_path:
                 if item.mtime != data[number] and data[number] > 0:
-                    result.append((file_path, item.mtime, ModificationType.MOD, item.is_dir))
+                    result_mod.append((file_path, item.mtime, ModificationType.MOD, item.is_dir))
                 break
         else:
             # are in files_map but already does not exist in file location
-            result.append((file_path, data[number], ModificationType.DEL, data[2]))
+            result_del.append((file_path, data[number], ModificationType.DEL, data[2]))
 
     diff = set(item2 for item2 in items2 if item2.name not in [item1[0] for item1 in items1])
     for item in diff:
         # are in file location but not in file_map
-        result.append((item.name, item.mtime, ModificationType.MK, item.is_dir))
+        result_mk.append((item.name, item.mtime, ModificationType.MK, item.is_dir))
 
-    return result
+    result_del.sort(key=lambda x: len(x[0]), reverse=True)
+    result_mk.sort(key=lambda x: len(x[0]))
+    return result_mod + result_del + result_mk
